a775fd39a2f74a433476588cce01a36c05c008ff
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-14 16:32:18 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -2,22 +2,22 @@ import logging
 from selenium import webdriver
 import os
 
-import sys
-sys.stdout.flush("Heeloooo Python")
-sys.stdout.flush("Heeloooo Python")
-sys.stdout.flush("Heeloooo Python")
-sys.stdout.flush("Heeloooo Python")
+#import sys
+#sys.stdout.flush("Heeloooo Python")
+#sys.stdout.flush("Heeloooo Python")
+#sys.stdout.flush("Heeloooo Python")
+#sys.stdout.flush("Heeloooo Python")
 
 
-driversPath = os.path.join(os.getcwd(),"\\Drivers\\chromedriver")
-chromeDriverPath = os.path.normpath(driversPath)
-print(chromeDriverPath)
-logging.info(chromeDriverPath)
+#driversPath = os.path.join(os.getcwd(),"\\Drivers\\chromedriver")
+#chromeDriverPath = os.path.normpath(driversPath)
+#print(chromeDriverPath)
+#logging.info(chromeDriverPath)
 
-driver = webdriver.Chrome(chromeDriverPath)
+#driver = webdriver.Chrome(chromeDriverPath)
 #driver = webdriver.Chrome()
-driver.get("http://www.google.co.in")
-driver.maximize_window()
+#driver.get("http://www.google.co.in")
+#driver.maximize_window()
 
 logging.basicConfig(level=logging.INFO)
 
