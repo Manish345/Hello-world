3f72a186daf1814e52c60df7f68901a98588feb5
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-14 12:36:28 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -2,12 +2,12 @@ import logging
 from selenium import webdriver
 import os
 
-#driversPath = os.path.join(os.getcwd(),"..\\..\\Drivers\\chromedriver")
-#chromeDriverPath = os.path.normpath(driversPath)
-#print(chromeDriverPath)
+driversPath = os.path.join(os.getcwd(),"\\Drivers\\chromedriver")
+chromeDriverPath = os.path.normpath(driversPath)
+print(chromeDriverPath)
 
-#driver = webdriver.Chrome(chromeDriverPath)
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(chromeDriverPath)
+#driver = webdriver.Chrome()
 driver.get("http://www.google.co.in")
 driver.maximize_window()
 
