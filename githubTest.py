93da34e5ea185ab62d392ac09720747052d5699c
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-14 17:42:14 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -1,6 +1,6 @@
 import logging
 from selenium import webdriver
-import os
+import os, sys
 
 driversPath = os.path.join(os.getcwd(),"\\Users\\manish2\\source\\Hello-world\\Drivers\\chromedriver")
 #driversPath = os.path.join(os.getcwd(),"\\Drivers\\chromedriver")
@@ -12,7 +12,7 @@ driver = webdriver.Chrome(chromeDriverPath)
 driver.get("http://www.google.co.in")
 driver.maximize_window()
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 print("GitHUb is working fine")
 logging.info("GitHUb is working fine")
