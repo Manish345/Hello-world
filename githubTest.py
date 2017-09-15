056b87a3d43df596d11a859837986ff40dc00083
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-15 12:11:41 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -2,16 +2,16 @@ import logging
 from selenium import webdriver
 import os, sys
 
-driversPath = os.path.join(os.getcwd(),"\\Users\\manish2\\source\\Hello-world\\Drivers\\chromedriver")
-#driversPath = os.path.join(os.getcwd(),"\\Drivers\\chromedriver")
+# driversPath = os.path.join(os.getcwd(),"\\Users\\manish2\\source\\Hello-world\\Drivers\\chromedriver")
+driversPath = os.path.join(os.getcwd(),"Drivers\\chromedriver")
 chromeDriverPath = os.path.normpath(driversPath)
 print(chromeDriverPath)
 logging.info(chromeDriverPath)
 
-driver = webdriver.Chrome(chromeDriverPath)
-driver.get("http://www.google.co.in")
-driver.maximize_window()
-print("Running Selenium code")
+# driver = webdriver.Chrome(chromeDriverPath)
+# driver.get("http://www.google.co.in")
+# driver.maximize_window()
+# print("Running Selenium code")
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
