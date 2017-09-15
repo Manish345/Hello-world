7c572bb3125968be32edb6437da8a015886c664f
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-15 12:59:23 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -2,7 +2,14 @@ import logging
 from selenium import webdriver
 import os, sys, pytest
 
-# driversPath = os.path.join(os.getcwd(),"\\Users\\manish2\\source\\Hello-world\\Drivers\\chromedriver")
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+# ch = logging.StreamHandler(sys.stdout)
+# ch.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# ch.setFormatter(formatter)
+root.addHandler(ch)
+
 driversPath = os.path.join(os.getcwd(),"Drivers\\chromedriver")
 chromeDriverPath = os.path.normpath(driversPath)
 print(chromeDriverPath)
@@ -13,28 +20,11 @@ logging.info(chromeDriverPath)
 # driver.maximize_window()
 # print("Running Selenium code")
 
-# logging.basicConfig(level=logging.INFO)
-# stderrLogger=logging.StreamHandler()
-# stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-# logging.getLogger().addHandler(stderrLogger)
-
-
-root = logging.getLogger()
-root.setLevel(logging.INFO)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logg = root.addHandler(ch)
 
 root.info("GitHUb is working fine")
 root.info("GitHUb is working fine")
 root.info("GitHUb is working fine")
 root.info("GitHUb is working fine")
-
-logg.info("GitHUb Loggs is working fine")
-logg.info("GitHUb Loggs is working fine")
-logg.info("GitHUb Loggs is working fine")
 
 print("GitHUb is working fine")
 logging.info("GitHUb is working fine")
