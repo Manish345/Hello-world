19c63cd861c610c4af0aa2f2d9564f76f16abf41
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-15 12:55:38 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -13,20 +13,31 @@ logging.info(chromeDriverPath)
 # driver.maximize_window()
 # print("Running Selenium code")
 
-logging.basicConfig(level=logging.INFO)
-stderrLogger=logging.StreamHandler()
-stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
-logging.getLogger().addHandler(stderrLogger)
+# logging.basicConfig(level=logging.INFO)
+# stderrLogger=logging.StreamHandler()
+# stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+# logging.getLogger().addHandler(stderrLogger)
+
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logg = root.addHandler(ch)
+
+root.info("GitHUb is working fine")
+root.info("GitHUb is working fine")
+root.info("GitHUb is working fine")
+root.info("GitHUb is working fine")
+
+logg.info("GitHUb Loggs is working fine")
+logg.info("GitHUb Loggs is working fine")
+logg.info("GitHUb Loggs is working fine")
 
 print("GitHUb is working fine")
 logging.info("GitHUb is working fine")
 logging.info("GitHUb is working fine")
 logging.info("GitHUb is working fine")
 
-
-def test_logging():
-  logging.info("Run test_logging()")
-  logging.info("Run test_logging()")
-  logging.info("Run test_logging()")
-  logging.info("Run test_logging()")
-pytest.main(["-s","githubTest.py"])
