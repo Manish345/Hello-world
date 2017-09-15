293a32ee1b039be1736d06a4ca455560ed0d8508
Manish345 <31850955+Manish345@users.noreply.github.com>
2017-09-15 14:09:09 +0530

    Update githubTest.py

--- githubTest.py
+++ githubTest.py
@@ -13,23 +13,10 @@ root.addHandler(ch)
 
 driversPath = os.path.join(os.getcwd(),"Drivers\\chromedriver")
 chromeDriverPath = os.path.normpath(driversPath)
-print(chromeDriverPath)
-logging.info("Chrome driver path is:")
-logging.info(chromeDriverPath)
 
 # driver = webdriver.Chrome(chromeDriverPath)
 # driver.get("http://www.google.co.in")
 # driver.maximize_window()
-# print("Running Selenium code")
-
-
-root.info("GitHUb is working fine")
-root.info("GitHUb is working fine")
-root.info("GitHUb is working fine")
-root.info("GitHUb is working fine")
-
-print("GitHUb print is working fine")
-logging.info("GitHUb logging is working fine")
-logging.info("GitHUb logging is working fine")
-logging.info("GitHUb logging is working fine")
+logging.info("Running Selenium code")
+logging.info("GitHUb is working fine with Jenkins")
 
